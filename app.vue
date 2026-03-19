f59b6d5290e1c334ab43ad2cb8b80c5cde6f8cac
SamSvindland <swsvindland@gmail.com>
2026-03-19 10:02:13 -0400

    example of loading state on convert

--- app.vue
+++ app.vue
@@ -6,6 +6,7 @@ const compressionFactor = ref<number>(1);
 const maxSize = ref<number>(2048);
 const maxFileSize = ref<number>(500);
 const outputType = ref("image/webp" as keyof typeof inputFileEndings);
+const isLoading = ref(false);
 
 const trimFileExtension = (filename: string) => {
   if (!filename) return "untitled_file_compressed";
@@ -16,12 +17,16 @@ const trimFileExtension = (filename: string) => {
 };
 const startConversion = async () => {
   if (file.value) {
+    isLoading.value = true;
     const reader = new FileReader();
 
     reader.onloadend = async (e) => {
       const res = e.target?.result;
 
-      if (!res || !file.value) return;
+      if (!res || !file.value) {
+        isLoading.value = false;
+        return;
+      }
 
       try {
         const params = {
@@ -45,6 +50,8 @@ const startConversion = async () => {
         }
       } catch (error) {
         console.error("Unexpected error during conversion:", error);
+      } finally {
+        isLoading.value = false;
       }
     };
 
@@ -100,9 +107,31 @@ const startConversion = async () => {
       <div class="flex justify-end">
         <button
           type="button"
-          class="flex items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 mt-5 focus:outline-none"
+          class="flex items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 mt-5 focus:outline-none disabled:bg-gray-500 disabled:cursor-not-allowed"
+          :disabled="isLoading"
           @click="startConversion"
         >
+          <svg
+            v-if="isLoading"
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            />
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            />
+          </svg>
           Convert
         </button>
       </div>
